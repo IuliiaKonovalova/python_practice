f9b141e0a34031d7a3d995ce936baebdb93306bd
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-08 14:27:33 +0800

    Solve day 1 task

---
+++ hackerRankBasic.py
@@ -0,0 +1,37 @@
+# Python if0else
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# if (n % 2 != 0):
+#     print('Weird')
+# else:
+#     if n in range(6, 21):
+#         print('Weird')
+#     else:
+#         print('Not Weird')
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+# j = int(input())
+j = 5
+e = 3.0
+t = 'here'
+# e = float(input())
+# t = input()
+# Print the sum of both integer variables on a new line.
+print(i+j)
+# Print the sum of the double variables on a new line.
+print(d+e)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+t)
