379c23defd8a8a73c370b4acd2f40844b668a8a8
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-10 14:50:33 +0800

    Solve list comprehensions problem

--- hackerRankBasic.py
+++ hackerRankBasic.py
@@ -194,3 +194,32 @@ def minion_game(string_word):
 
 
 minion_game('BANANA')
+
+
+
+###########################
+##                       ##
+##  List Comprehensions  ##
+##                       ##
+###########################
+
+
+x = 1
+y = 2
+z = 2
+n = 3
+    
+print ([[i, j, k] for i  in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n ])
+
+
+
+
+
+
+#########################
+##                     ##
+##   Merge the Tools   ##
+##                     ##
+#########################
+
+
