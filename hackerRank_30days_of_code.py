4d1df40792fbaaa6e91cf18066df9903304eb68b
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-11 18:56:03 +0800

    Solve day 4 problem

--- hackerRank_30days_of_code.py
+++ hackerRank_30days_of_code.py
@@ -37,3 +37,28 @@ print(solveMeal(100, 15, 8))
 
 N = 5
 print('Weird' if N % 2 != 0 or 6 <= N <= 20 else "Not Weird")
+
+# Day 4
+
+class Person:
+    def __init__(self,initialAge):
+        self.initialAge = initialAge
+        self.age = 0
+        if initialAge < 0:
+          print('Age is not valid, setting age to 0.')
+        else:
+          self.age = initialAge
+        # Add some more code to run some checks on initialAge
+    def amIOld(self):
+        if self.age < 13:
+            print("You are young.")
+        elif 13 <= self.age < 18:
+          print("You are a teenager.")
+        elif self.age >= 18:
+          print("You are old.")
+        # Do some computations in here and print out the correct statement to the console
+    def yearPasses(self):
+
+        global age
+        self.age += 1
+        # Increment the age of the person in here
