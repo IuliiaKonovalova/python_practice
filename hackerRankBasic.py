0fddae40b675f31ed2c82d602310818f55556266
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-14 17:04:57 +0800

    Solve any or all challenge

--- hackerRankBasic.py
+++ hackerRankBasic.py
@@ -748,3 +748,17 @@ OrderedCounter(OrderedDict([('a', 5), ('d', 6), ('b', 2), ('r', 2), ('c', 1)]))
 #     for el in arr:
 #           # unpacking
 #         print(*el)
+
+
+
+###################################################
+##                                               ##
+##                 Any or all                    ##
+##                                               ##
+###################################################
+
+# amount_of_numbers = input()
+
+# numbers = list(map(int, input().split()))
+
+# print(all([any([str(element) == str(element)[::-1] for element in numbers]), any([int(element) > 0 for element in numbers])]))
