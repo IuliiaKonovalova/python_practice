c06dcb7ed4209fbce449eef7490e4ba754258dcc
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-16 19:58:00 +0800

    Solve set descard remove pop challenge

--- hackerRankBasic.py
+++ hackerRankBasic.py
@@ -1001,6 +1001,22 @@ def average(array):
 
 ###################################################
 ##                                               ##
-##                   Set.add()                   ##
+##       Set .discard(), .remove() & .pop()      ##
 ##                                               ##
 ###################################################
+
+
+num = int(input())
+data = set(map(int, input().split()))
+operations = int(input())
+
+for x in range(operations):
+  operation = input().split()
+  if operation[0] == "remove":
+    data.remove(int(operation[1]))
+  elif operation[0] == "discard":
+    data.discard(int(operation[1]))
+  else:
+    data.pop()
+    
+print(sum(data))
