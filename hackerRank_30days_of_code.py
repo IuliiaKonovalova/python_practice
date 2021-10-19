21e1b8f793250725cf257a849b942e0defd4ce80
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-19 11:40:16 +0800

    Solve 11 day challenge

--- hackerRank_30days_of_code.py
+++ hackerRank_30days_of_code.py
@@ -166,3 +166,33 @@ while n > 0:
         current_consecutive_ls = 0
     n = n // 2
 print(max_consecutive_ls)
+
+# Day 11
+
+grid = list()
+
+for i in range(6):
+    row = input().strip().split(' ')
+    row = list(map(int, row))
+    grid.append(row)
+
+def _get_hourglass_sum(grid, i, j):
+    sum = 0
+    sum += grid[i-1][j-1]
+    sum += grid[i-1][j]
+    sum += grid[i-1][j+1]
+    sum += grid[i][j]
+    sum += grid[i+1][j-1]
+    sum += grid[i+1][j]
+    sum += grid[i+1][j+1]
+    return sum
+
+# start max_hourglass_sum at smallest possible hourglass
+max_hourglass_sum = -63
+for i in range(1,5):
+    for j in range(1, 5):
+        current_hourglass_sum = _get_hourglass_sum(grid, i, j)
+        if current_hourglass_sum > max_hourglass_sum:
+            max_hourglass_sum = current_hourglass_sum
+
+print(max_hourglass_sum)
