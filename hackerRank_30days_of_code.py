6cb343bac04995f09df1a75646450fa006890c85
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-22 19:40:05 +0800

    Solve 15 day challenge

--- hackerRank_30days_of_code.py
+++ hackerRank_30days_of_code.py
@@ -304,3 +304,37 @@ new_novel.display()
 # d.computeDifference()
 
 # print(d.maximumDifference)
+
+# Day 15
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        new_node = Node(data)
+        if head is None:
+            self.head = new_node
+            return self.head
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        return self.head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 
