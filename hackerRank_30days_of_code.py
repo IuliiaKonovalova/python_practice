166dc7bbb175ed5c7c623aa752eba35dcdd7a3fe
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-26 19:07:14 +0800

    Solve day 19 challenge

--- hackerRank_30days_of_code.py
+++ hackerRank_30days_of_code.py
@@ -423,3 +423,25 @@ if isPalindrome:
     print("The word, "+s+", is a palindrome.")
 else:
     print("The word, "+s+", is not a palindrome.")    
+
+
+# Day 19: Interfaces
+
+
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        temp = []
+        for i in range(1, n+1):
+            if n%i == 0:
+                temp.append(i)
+        return sum(temp)
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
