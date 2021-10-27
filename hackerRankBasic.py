2354edc58b6cfb807b3b35a867b614b6036e047c
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-27 08:21:41 +0800

    Solve maximize it challenge

--- hackerRankBasic.py
+++ hackerRankBasic.py
@@ -1517,6 +1517,14 @@ print("{0:.3}".format(len(list(F))/len(C)))
 
 ###################################################
 ##                                               ##
-##            Iterables and Iterators            ##
+##                     Maximize it               ##
 ##                                               ##
 ###################################################
+
+
+from itertools import product
+
+K,M = map(int,input().split())
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
+print(max(results))
