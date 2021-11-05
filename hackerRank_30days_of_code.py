cc0a80184dcd5bc7f4cc16c3f4de6fd3fe155ac1
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-05 14:40:11 +0800

    Add solution to the 26 day challange

--- hackerRank_30days_of_code.py
+++ hackerRank_30days_of_code.py
@@ -649,3 +649,27 @@ for i in range(number_of_tests):
         print('Prime')
     else:
         print('Not prime')
+
+# Day 26 Nested Logic |
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+return_day, return_month, return_year = [int(num) for num in input().split(' ')]
+
+due_day, due_month, due_year = [int(num) for num in input().split(' ')]
+
+if return_year < due_year:
+    print(0)
+elif return_year == due_year:
+    if return_month < due_month:
+        print(0)
+    elif return_month == due_month:
+        if return_day < due_day:
+            print(0)
+        else:
+            print(15 * (return_day - due_day))
+    else:
+        print(500 * (return_month - due_month))
+        
+else:
+    print(10000)
