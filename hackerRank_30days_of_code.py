327a5c29080ac5802238bae3a63365996471286f
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-05 15:41:56 +0800

    Solve day 28 challange

--- hackerRank_30days_of_code.py
+++ hackerRank_30days_of_code.py
@@ -743,3 +743,33 @@ TestWithEmptyArray()
 TestWithUniqueValues()
 TestiWithExactyTwoDifferentMinimums()
 print("OK")
+
+
+# Day 28: RegEx, Patterns, and Intro to Databases
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    first_name_list = []
+    pattern = r"@gmail\.com"
+    regex = re.compile(pattern)
+    for N_itr in range(N):
+        first_multiple_input = input().rstrip().split()
+
+        firstName = first_multiple_input[0]
+        emailID = first_multiple_input[1]
+        if regex.search(emailID):
+            first_name_list.append(firstName)
+
+    first_name_list.sort()
+    
+    for name in first_name_list:
+        print(name)
